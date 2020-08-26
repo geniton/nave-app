df0de7d0212bebad184eb151a74fc653c84b6cc9
Geniton Lima <geniton.lima@visie.com.br>
2020-08-26 07:37:37 -0300

    Create validUrl,validName and required

--- src/components/FormValidation/FormValidation.js
+++ src/components/FormValidation/FormValidation.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+const validUrl = value => /^(ftp|http|https):\/\/[^ "]+$/.test(value)
+const validName = value => /^[a-z][a-z\s]*$/gim.test(value);
 const validEmail = value => /^[\w._+-]+(\.\w+)?@\w+\.[\w]+(\.[\w]+)?$/gim.test(value);
 const validMinlength = (value, length) => value.length >= parseInt(length);
 
@@ -26,12 +28,18 @@ const FormValidation = (props) => {
 		$validatedInputs.forEach($input => {
       const validation = $input.getAttribute("data-validation");
 			const inputName = $input.getAttribute("name");
-			const inputValue = $input.value.trim();
-
+      const inputValue = $input.value.trim();
+      
+      if (/name/.test(validation) && !validName(inputValue))
+      updateFromErrorState(inputName);
+      if (/url/.test(validation) && !validUrl(inputValue))
+      updateFromErrorState(inputName);
 			if (/email/.test(validation) && !validEmail(inputValue))
         updateFromErrorState(inputName);
       if (/minlength/.test(validation) && !validMinlength(inputValue,$input.getAttribute("data-min-length")))
         updateFromErrorState(inputName);
+      if (/required/.test(validation) && (!inputValue || inputValue.length <= 0))
+      updateFromErrorState(inputName);
     });
 
 	  props.onSubmit(e, isValid, formError);
