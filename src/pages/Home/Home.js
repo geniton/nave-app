c611c5d9f5fdb93a9c89178e4853e45b00b8109a
Geniton Lima <geniton.lima@visie.com.br>
2020-08-25 22:43:49 -0300

    Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import Api from 'services/Api'
+
+// components
+import Card from 'components/Card'
+
+// styles 
+
+import {
+  NaveContainer,
+  NaveHeader,
+  NaveCards,
+  Title,
+  Button
+} from './styles'
 
 function Home() {
-  return <div />;
+  const [users, setUsers] = useState(null)
+
+  useEffect(() => {
+      (async () => {
+          const { data } = await Api.get('/navers')
+          setUsers(data)
+      })()
+  },[])
+  
+  return (
+   users ? 
+    <NaveContainer>
+      <NaveHeader>
+        <Title>Navers</Title>
+        <Button>Adicionar Naver</Button>
+      </NaveHeader>
+      <NaveCards>
+        { users.map(user => <Card key={user.id} user={user}/> )}
+      </NaveCards>
+    </NaveContainer> : null
+  );
 }
 
 export default Home;
