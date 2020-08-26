47453395ce639deca97dd85192b41966867f1513
Geniton Lima <geniton.lima@visie.com.br>
2020-08-25 22:32:42 -0300

    Create routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,31 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { AuthContext } from 'contexts/AuthProvider'
 import Login from 'pages/Login'
+import Home from 'pages/Home'
+import Profile from 'pages/Profile'
 
-export default function Routes () {
+const CustomRoute = ({ isPrivate, ...rest }) => {
+  const { authenticated, loading } = useContext(AuthContext)
+
+  if (loading) {
+    return false
+  }
+
+  if (isPrivate && !authenticated) {
+    return <Redirect to="/login"/>
+  }
+
+  return <Route {...rest} />
+}
+
+export default function Routes() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path='/' exact component={Login} />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <CustomRoute isPrivate exact path="/" component={Home}/>
+      <CustomRoute exact path="/login" component={Login}/>
+      <CustomRoute isPrivate exact path="/perfil" component={Profile}/>
+    </Switch>
   )
 }
+
