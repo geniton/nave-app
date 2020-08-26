804ecb7da0d866f6207d59f6bf7be0124ea8ae41
Geniton Lima <geniton.lima@visie.com.br>
2020-08-25 22:37:53 -0300

    add img ul and tags title

--- src/layouts/globalStyles.js
+++ src/layouts/globalStyles.js
@@ -12,6 +12,21 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     font-family: ${props => props.theme.font}
   }
+
+  img {
+    max-width: 100%;
+    display: block;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  h1,h2,h3,h4,h5,h6,p{
+    margin: 0;
+  }
 `;
  
 export default GlobalStyle;
