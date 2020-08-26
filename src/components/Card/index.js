f4184e6bab0dd38aa6b33174320206c7732bf9c2
Geniton Lima <geniton.lima@visie.com.br>
2020-08-25 22:31:57 -0300

    Create card home

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,19 +1,2 @@
-import React, { useEffect } from 'react';
-import { Content } from './styles'
-
-// import { Container } from './styles';
-
-function Card({data}) {
-  return (
-    <CardContent>
-      <figure>
-        <img src={data.url} alt=""/>
-      </figure>
-      <h3>{data.name}</h3>
-      <p>{data.jog_role}</p>
-      <ToolsList/>
-    </CardContent>
-  )
-}
-
+import { default as Card } from './Card'
 export default Card;
