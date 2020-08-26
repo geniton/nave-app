f76cb7b07ebe3d834df8717248c20b3a0ea7f0fd
Geniton Lima <geniton.lima@visie.com.br>
2020-08-25 22:33:29 -0300

    Card styles

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -2,6 +2,20 @@ import styled from 'styled-components'
 
 export const CardContent = styled.div`
   figure {
-    max-height: 280px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    margin-bottom: 15px;
+    cursor: pointer;
+  }
+
+  h3 {
+    font-size: 16px;
+    margin-bottom: 10px;
+  }
+
+  p {
+    margin-bottom: 15px;
   }
 `
